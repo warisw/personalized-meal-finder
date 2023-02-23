8634b14e6305c79a3d2c72107c860d32e9e18087
Aris Bousios <aris.bousios@newcrosshealthcare.com>
2023-02-23 13:28:41 +0200

    project cleanup + seach page init

---
+++ front-end/src/main-page/Main.style.ts
@@ -0,0 +1,70 @@
+import { LabelHTMLAttributes } from "react";
+import styled, { css } from "styled-components";
+import { Input } from "@mui/material";
+
+export const Container = styled.div`
+  ${() => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    background-color: #b5f1cc;
+  `};
+`;
+export const InnerContainer = styled.div`
+  ${() => css`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    flex-direction: column;
+  `};
+`;
+
+export const InputContainer = styled.div`
+  ${() => css`
+    display: flex;
+    justify-content: flex-start;
+
+    padding: 1rem;
+    width: 80%;
+    border-radius: 25px;
+    background-color: #fcc2fc;
+  `};
+`;
+
+export const Label = styled.div<LabelHTMLAttributes<HTMLLabelElement>>`
+  ${() => css`
+    padding: 1rem;
+    font-size: 24px;
+    font-family: "Indie Flower";
+  `};
+`;
+
+export const CustomInput = styled(Input)`
+  ${() => css`
+    padding: 0.5rem;
+    font-size: 22px !important;
+    font-family: "Lato" !important;
+    font-style: italic;
+    width: 80%;
+  `};
+`;
+
+export const Title = styled.div`
+  ${() => css`
+    font-family: "Indie Flower";
+    font-size: 3rem;
+    margin-bottom: 3rem;
+  `};
+`;
+export const Header = styled.div`
+  ${() => css`
+    font-family: "Indie Flower";
+    font-size: 3rem;
+    background-color: #e5fdd1;
+    width: 100%;
+    position: fixed;
+    top: 0;
+  `};
+`;
---
+++ front-end/src/main-page/Main.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import * as Styled from "./Main.style";
+function Main() {
+  const [inputData, setInputData] = useState("");
+
+  return (
+    <Styled.Container>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap"
+      />
+      <Styled.Header>
+        <Button onClick={() => console.log("clicked")}>logout</Button>
+      </Styled.Header>
+      <Styled.InnerContainer>
+        <Styled.Title>Find your mmMeal ..!</Styled.Title>
+        <Styled.InputContainer>
+          <Styled.Label htmlFor="user-input-data">Enter data </Styled.Label>
+          <Styled.CustomInput
+            type="text"
+            placeholder="tomato,3 potatos, onions, greek"
+            name="user-input-data"
+            onChange={(event) => setInputData(event?.target.value)}
+          />
+          <Button onClick={() => console.log("clicked")}>search</Button>
+        </Styled.InputContainer>
+      </Styled.InnerContainer>
+    </Styled.Container>
+  );
+}
+
+export default Main;
